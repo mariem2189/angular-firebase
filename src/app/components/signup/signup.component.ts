cd690f174e97582301df62eeaf12c84ae4417c3c
miranda <miranda@0D-pro-urbanisme.DC-CAPES.COM>
2021-07-08 13:32:39 +0200

    second commit

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -11,7 +11,7 @@ export class SignupComponent implements OnInit {
   email: string
   password: string
 
-  constructor(private authenticationService: AuthService) { }
+  constructor(public authenticationService: AuthService) { }
 
   ngOnInit() {
 
